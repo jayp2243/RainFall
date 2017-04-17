05ecd641b95e47ef0b20cfb8feb7a2afedbc1b8c
jayp2243 <jayp2243@gmail.com>
2017-04-17 19:48:32 -0400

    Update and rename Rainfall to Rainfall.java

--- Rainfall.java
+++ Rainfall.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-public class RainfallAmounts {
+public class Rainfall {
 
 public static void main(String[] args) {
 	//See instructions above.
